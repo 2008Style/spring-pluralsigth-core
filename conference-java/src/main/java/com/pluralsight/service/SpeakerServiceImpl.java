fbbcce8b36f33b4da9d687f2a29cd523e1a44d24
Jose <Jose-Antonio.RUIZ-JIMENEZ@akkodis.com>
2023-08-24 13:27:18 +0200

    -Advanced Bean Configuration

--- conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,11 +3,14 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
@@ -20,6 +23,11 @@ public class SpeakerServiceImpl implements SpeakerService {
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository constructor");
         this.repository = repository;
+    }
+
+    @PostConstruct
+    private void initializa() {
+        System.out.println("We're called after the constructors");
     }
 
     @Override
---
+++ conference-xml/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.pluralsight.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
